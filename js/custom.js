9c4a030689634e772a4bf94d2a1e7bfa22ca7234
kawtar1993 <mansourikawtar93@gmail.com>
2019-03-28 16:44:40 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+//when the link preferred shops is clicked
+function preferred(){
+    event.preventDefault();
+	$(".shops").hide();
+	$(".pshops").show();
+}
+//when the link register is clicked
+function register(){
+    event.preventDefault();
+	$(".log").hide();
+	$(".register").show();
+}
+	
